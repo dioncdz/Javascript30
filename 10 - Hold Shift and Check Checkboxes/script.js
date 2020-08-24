c79f4f3a04cfbfe7265ffc2cc84f1b9f7e671e74
dioncdz <dioncadizjr@gmail.com>
2020-08-24 15:31:46 +0800

    Day10 JS30

---
+++ 10 - Hold Shift and Check Checkboxes/script.js
@@ -0,0 +1,32 @@
+const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
+console.log(checkboxes);
+
+let lastChecked;
+
+function handleCheck(e) {
+   // check if they had the shift key down
+   // and check that they are checking the checkbox
+   let inBetween = false;
+   
+   if(e.shiftKey && this.checked) {
+      // loop over every single checkbox
+      checkboxes.forEach(checkbox => {
+         console.log(checkbox);
+         if(checkbox === this || checkbox === lastChecked) {
+            inBetween = !inBetween;
+            console.log('Starting to check them inBetween');
+         }
+
+         if(inBetween) {
+            checkbox.checked = true;
+         }
+      })
+   }
+   lastChecked = (this);
+}
+
+
+checkboxes.forEach((checkbox)=> {
+   checkbox.addEventListener('click', handleCheck);
+   
+})
